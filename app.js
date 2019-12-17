2bd32e0f67d81d29f22332dbbaab823beb1241e4
Aman Sultan Baig <amansultanbaig@gmail.com>
2019-12-17 22:56:01 +0500

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var http = require('http');
 http.createServer(function (req, res) {
   
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write('Hello World!'); //write a response to the client
+  res.write('<h1>Hello World!</h1>'); //write a response to the client
   res.end(); //end the response
   
 }).listen(3000); //the server object listens on port 3000
